689ad713f8c02c6a855cd43bf56248927ee3f98a
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-03-10 21:08:02 +0100

    add colors

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,17 @@
 const ACCESS_TOKEN =  "1cc7e414-8575-3a03-9ca7-7f705c98c62c"
 
-export const URL = "https://apigw.nubentos.com:443/t/nubentos.com/ncovapi/1.0.0/"
+export const URL = "https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/"
+
+export const colors = {
+    deaths: 'red',
+    confirmed: 'skyblue',
+    recovered: 'green'
+}
 
 export const config = {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${ACCESS_TOKEN}`
     },
 }
