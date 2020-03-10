28f8bbf758d23b25401de07841ee6217b344f4db
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-03-10 20:57:49 +0100

    chng api

--- util/databank.js
+++ util/databank.js
@@ -1,31 +1,8 @@
 import { URL, config } from "../config/keys";
 
-export async function fetchCases(country){
-    const newCases = await fetch(`${URL}cases/confirmed`, config)
+export async function fetchBrief() {
+    const newCases = await fetch(`${URL}jhu-edu/brief`, config)
     const cases = await newCases.json();
     console.warn(cases);
-}
-
-export async function fetchDeaths() {
-    const newCases = await fetch(`${URL}deaths`, config)
-    const cases = await newCases.json();
-    console.warn(cases);
-}
-
-export async function fetchReecovered() {
-    const newCases = await fetch(`${URL}recovered`, config)
-    const cases = await newCases.json();
-    console.warn(cases);
-}
-
-export async function fetchAll() {
-    const newCases = await fetch(`${URL}cases`, config)
-    const cases = await newCases.json();
-    console.warn(cases);
-}
-
-export async function fetchSuspected() {
-    const newCases = await fetch(`${URL}cases/suspected`, config)
-    const cases = await newCases.json();
-    console.warn(cases);
+    return cases;
 }
