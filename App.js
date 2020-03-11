f6565e02be03886c147449d02ca360124c4c9799
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-03-11 10:33:21 +0100

    inst nav

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Brief } from './screens/Brief';
+import React from 'react';
+import { AppNav } from './navigation/appnav';
 
 export default function App() {
   return (
-    <Brief/>
+    <AppNav/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
